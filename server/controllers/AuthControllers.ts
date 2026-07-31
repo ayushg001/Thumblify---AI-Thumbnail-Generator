535f25097713045cd5a31b892c451993c6138031
Ayush Gupta <ayushguptanonmedical@gmail.com>
2026-08-01 00:56:24 +0530

    setup the backend , created the controller for userAuth

---
+++ server/controllers/AuthControllers.ts
@@ -0,0 +1,95 @@
+import {Request , Response} from 'express';
+import User from '../models/User.js';
+import bcrypt from 'bcrypt';
+
+// Controllers for user registration
+export const registerUser = async (req : Request , res : Response) => {
+   try{
+        const {name , email , password} = req.body;
+        // Find User by Email
+        const user = await User.findOne({email});
+        if(user){
+            return res.status(400).json({message :  'User already exists'})
+        }
+        // Encrypt the password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password , salt);
+        // Save the User
+        const newUser = new User({name , email , password : hashedPassword});
+        await newUser.save();
+       // Setting user data in session
+       req.session.isLoggedIn = true;
+       req.session.userId = newUser._id;
+
+       return res.json({
+            message :  'Account created successfully',
+            user : {
+                _id : newUser._id,
+                name : newUser.name,
+                email : newUser.email
+            }
+       })
+   } catch (error : any){
+        console.log("Error during Register : " , error);
+        res.status(500).json({message : error.message})
+   }
+}
+
+export const loginUser = async (req : Request , res : Response) => {
+   try{
+        const {email , password} = req.body;
+        // Find User by Email
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({message :  'Invalid email or password'})
+        }
+        // Check the password
+        const isPasswordCorrect = await bcrypt.compare(password , user.password);
+        if(!isPasswordCorrect){
+                return res.status(400).json({message :  'Invalid email or password'})
+            }
+       // Setting user data in session
+       req.session.isLoggedIn = true;
+       req.session.userId = user._id;
+
+       return res.json({
+            message :  'Login successfully',
+            user : {
+                _id : user._id,
+                name : user.name,
+                email : user.email
+            }
+       })
+   } catch (error : any){
+        console.log("Error during Register : " , error);
+        res.status(500).json({message : error.message})
+   }
+}
+
+// LogOut
+export const logoutUser = async (req : Request , res : Response) => {
+    req.session.destroy( (error : any)=> {
+            if(error){
+                console.log("Error in Logout : ",error)
+                return res.status(500).json({message : error.message})
+            }
+    })
+    return res.json({message : 'Logout successfully'})
+}
+
+export const verifyUser = async (req : Request , res : Response) => {
+    try {
+        const {userId} = req.session;
+
+        const user = await User.findById(userId).select("-password");
+        if(!user){
+            res.status(400).json({message : 'Invalid User'})
+        }
+
+        return res.json({user})
+
+     } catch (error : any){
+        console.log("Error during Register : " , error);
+        res.status(500).json({message : error.message})
+   }
+}
