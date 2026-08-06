3ea1ad4b80f98a13ac6368465adeec0b2dccb90b
Ayush Gupta <ayushguptanonmedical@gmail.com>
2026-08-07 01:19:05 +0530

    .

--- server/server.ts
+++ server/server.ts
@@ -20,7 +20,7 @@ await connectDB();
 const app = express();
 
 app.use(cors({
-    origin : ['http://localhost:5173' , 'http://localhost:3000'],
+    origin : ['http://localhost:5173' , 'http://localhost:3000' , "http://51.20.149.65:5173"],
     credentials : true
 }));
 
